fb1548f1d1d2010dbeeb686ea3bc674dfd53d370
eagonzalez81 <gonzoatc81@gmail.com>
2022-07-14 19:52:06 -0400

    create item post reply rest models

---
+++ models/restaurant.js
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+
+const Schema = mongoose.Schema
+
+const restaurantSchema = new Schema({
+  title: {type: String, required: true},
+  category: String
+}, {
+  timestamps: true
+})
+
+const Restaurant = mongoose.model('Restaurant', restaurantSchema)
+
+export {
+  Restaurant
+}

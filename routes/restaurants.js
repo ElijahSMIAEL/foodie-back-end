4fd7f93c7ac3beca3187a0418f3f4574197faced
Endrews <elijah.smiael@gmail.com>
2022-07-15 10:55:29 -0500

    stub up basic restaurant components

---
+++ routes/restaurants.js
@@ -0,0 +1,14 @@
+import { Router } from 'express'
+import * as restaurantsCtrl from '../controllers/restaurants'
+import { decodeUserFromToken, checkAuth } from '../middleware/auth.js'
+
+const router = Router()
+
+/*---------- Public Routes ----------*/
+
+
+/*---------- Protected Routes ----------*/
+router.use(decodeUserFromToken)
+router.get('/search/:restaurantName', checkAuth, restaurantsCtrl.search)
+
+export { router }

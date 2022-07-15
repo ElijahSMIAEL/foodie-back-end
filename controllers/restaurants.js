b4b429be582cf35f6c294e625d4fb68f758ba3a5
Endrews <elijah.smiael@gmail.com>
2022-07-15 13:09:11 -0500

    add backend api-search-query

--- controllers/restaurants.js
+++ controllers/restaurants.js
@@ -1,8 +1,20 @@
+import axios from 'axios'
+import { config } from 'dotenv'
+import { response } from 'express'
 import { Profile } from '../models/profile.js'
 import { Restaurant } from '../models/restaurant.js'
 
+const apiKey = `${process.env.API_KEY}`
+const apiBaseUrl = 'https://api.yelp.com/v3/businesses'
+
 function search(req, res) {
-  
+  axios.get(`${apiBaseUrl}/search?term=${userInput}&location=${userlocation}&categories=food`, config)
+    .then((res) => {
+      console.log(res)
+    })
+    .catch((err) => {
+      console.log(err)
+    })
 }
 
 

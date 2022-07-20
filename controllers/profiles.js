26654eac0dfdd185a223557d2c288360b24b5b07
Endrews <elijah.smiael@gmail.com>
2022-07-20 08:25:37 -0500

    add server functionality for profile photo

--- controllers/profiles.js
+++ controllers/profiles.js
@@ -1,4 +1,5 @@
 import { Profile } from '../models/profile.js'
+import { v2 as cloudinary } from 'cloudinary'
 
 function index(req, res) {
   Profile.find({})
@@ -32,8 +33,28 @@ function update(req, res) {
   })
 }
 
+function addPhoto(req, res) {
+  const imageFile = req.files.photo.path
+  Profile.findById(req.params.id)
+  .then(profile => {
+    cloudinary.uploader.upload(imageFile, {tags: `${profile.avatar}`})
+    .then(image => {
+      profile.profilePhoto = image.url
+      profile.save()
+      .then(profile => {
+        res.status(201).json(profile.profilePhoto)
+      })
+    })
+    .catch(err => {
+      console.log(err)
+      res.status(500).json(err)
+    })
+  })
+}
+
 export { 
   index, 
   show,
-  update, 
+  update,
+  addPhoto, 
 }
--- models/profile.js
+++ models/profile.js
@@ -4,6 +4,7 @@ const profileSchema = new mongoose.Schema({
   email: { type: String, required: true, lowercase: true, unique: true },
   name: String,
   bio: String,
+  profilePhoto: String,
 },{
   timestamps: true,
 })
